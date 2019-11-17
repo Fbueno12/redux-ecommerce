7dd46d646457fa0f355f898ac68e93551b6d92c0
Felipe Bueno <felipebueno181@gmail.com>
2019-11-17 20:20:33 -0300

    estilizando o carrinho de compras

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,74 @@
 import React from 'react';
+import {
+    MdRemoveCircleOutline,
+    MdAddCircleOutline,
+    MdDelete,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 // import { Container } from './styles';
 
 export default function Cart() {
-    return <div>CART</div>;
+    return (
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>Produto</th>
+                        <th>Quantidade</th>
+                        <th>Subtotal</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img
+                                src="https://images-na.ssl-images-amazon.com/images/I/91bQYhS1yQL._UX500_.jpg"
+                                alt="Tenis"
+                            />
+                        </td>
+                        <td>
+                            <strong>Tênis muito massa</strong>
+                            <span>R$129,90</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                                <input type="number" readOnly value={2} />
+                                <button type="button">
+                                    <MdAddCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$258,80</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
+            <footer>
+                <button type="button">Finalizar pedido</button>
+
+                <Total>
+                    <span>TOTAL</span>
+                    <strong>R$1920,28</strong>
+                </Total>
+            </footer>
+        </Container>
+    );
 }
