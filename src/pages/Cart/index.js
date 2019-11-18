8334168192eae7a34efc72dab2c10fbb6b2804f5
Felipe Bueno <felipebueno181@gmail.com>
2019-11-17 21:55:17 -0300

    removendo produto do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -10,7 +10,7 @@ import { Container, ProductTable, Total } from './styles';
 
 // import { Container } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
     return (
         <Container>
             <ProductTable>
@@ -58,7 +58,15 @@ function Cart({ cart }) {
                                 <strong>R$258,80</strong>
                             </td>
                             <td>
-                                <button type="button">
+                                <button
+                                    type="button"
+                                    onClick={() =>
+                                        dispatch({
+                                            type: 'REMOVE_FROM_CART',
+                                            id: product.id,
+                                        })
+                                    }
+                                >
                                     <MdDelete size={20} color="#7159c1" />
                                 </button>
                             </td>
