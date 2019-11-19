2d34071b06cd107c6d7c7c7e0710e552616ecb26
Felipe Bueno <felipebueno181@gmail.com>
2019-11-18 21:15:09 -0300

    redux-saga no update

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -7,16 +7,16 @@ import {
     MdDelete,
 } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
-import { Container, ProductTable, Total } from './styles';
 import * as CartActions from '../../store/modules/cart/actions';
+import { Container, ProductTable, Total } from './styles';
 // import { Container } from './styles';
 
-function Cart({ cart, total, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
     function increment(product) {
-        updateAmount(product.id, product.amount + 1);
+        updateAmountRequest(product.id, product.amount + 1);
     }
     function decrement(product) {
-        updateAmount(product.id, product.amount - 1);
+        updateAmountRequest(product.id, product.amount - 1);
     }
 
     return (
